fda8dca5f684f7b25bc5485f20e6243db70ba31e
kleftogiannis <kleftogiannis@oktopay.eu>
2024-12-06 17:26:27 +0200

    added button and nav

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import { MagicButton } from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
@@ -17,8 +19,12 @@ const Hero = () => {
         <div className='flex justify-center relative my-20 z-10'>
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                 <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic web magic with Next.js</h2>
-                <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl ' words='Transforming Concepts into Seamless Experiences'/>
+                <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl ' words='Transforming Concepts into Seamless User Experiences'/>
                 <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>Hi, I&apos;m Ioannis, a Next.js developer from Greece</p>
+
+                <a href="#about">
+                  <MagicButton title="Show my work" icon={<FaLocationArrow />} position={"right"} />
+                </a>
             </div>
         </div>
     </div>
